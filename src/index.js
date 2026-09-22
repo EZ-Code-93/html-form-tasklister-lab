4122045ebfbcdf760ef13e33987033aae396bbcf
EZ-Code-93 <Ezra.93@live.com>
2026-09-22 11:32:29 -0600

    funtion for validating html form and adding a new element for updating a task list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description")
+    const taskDescription = taskInput.value;
+
+    buildToDo(taskDescription);
+  });
+  
+
+  function buildToDo(task) {
+    const newLi = document.createElement("li");
+    newLi.textContent = task;
+
+    const taskList = document.getElementById("tasks");
+    taskList.append(newLi);
+  }
 });
